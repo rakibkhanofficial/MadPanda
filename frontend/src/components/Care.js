8d546617212474b67ce41a3d78bba29a0fa02dbc
Rakib Khan <39366407+Rakib00111@users.noreply.github.com>
2022-08-05 17:22:05 +0600

    Add files via upload
    MedPanda is e-commerce based project using react js node js me and my team mate made it for our college degree , This website is actually a Medicine delivery system which provide add to card features, log in account create account , payment method using E-wallet also using credit card debit card .

---
+++ frontend/src/components/Care.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Care = () => {
+  return (
+    <>
+        <h1><b>5 health tips for 2020</b></h1>
+        <i><mark>1. Eat a healthy diet
+        </mark></i>
+
+        <div>Eat a combination of different foods, including fruit, vegetables, legumes, nuts and whole grains. 
+          Adults should eat at least five portions (400g) of fruit and vegetables per day. 
+          You can improve your intake of fruits and vegetables by always including veggies in your meal; 
+          eating fresh fruit and vegetables as snacks; eating a variety of fruits and vegetables; and eating them in season. 
+          By eating healthy, you will reduce your risk of malnutrition and noncommunicable diseases (NCDs) such as diabetes, heart disease, stroke and cancer.
+          </div>
+
+        
+        <i><mark>2. Consume less salt and sugar
+        </mark></i>
+
+        <div>consume twice the recommended amount of sodium, putting them at risk of high blood pressure, which in turn increases the risk of heart disease and stroke. Most people get their sodium through salt. Reduce your salt intake to 5g per day, equivalent to about one teaspoon. 
+          It’s easier to do this by limiting the amount of salt, soy sauce, fish sauce and other high-sodium condiments when preparing meals; 
+          removing salt, seasonings and condiments from your meal table; 
+          avoiding salty snacks; and choosing low-sodium products.</div>
+    </>
+
+  )
+}
+
+export default Care
